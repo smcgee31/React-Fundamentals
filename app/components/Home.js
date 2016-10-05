3d624586ac40ae78ebf9e7ca8757230684e91f92
smcgee31 <solodesign31@gmail.com>
2016-10-04 20:59:05 -0600

    renamed some files and changed corresponding exports for clarity

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,17 +2,17 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 var transparentBg = require('../styles').transparentBg;
-var MainContainer = require('./MainContainer');
+var HomeWrapper = require('./HomeWrapper');
 
 function Home () {
   return (
-    <MainContainer>
+    <HomeWrapper>
       <h1>Github Battle</h1>
       <p className='lead'>What even is a jQuery?</p>
       <Link to='/playerOne'>
         <button type='button' className='btn btn-lg btn-success'>Get Started</button>
       </Link>
-    </MainContainer>
+    </HomeWrapper>
   )
 }
 
